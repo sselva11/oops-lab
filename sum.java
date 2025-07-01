28e3f151a7d27e2bed058c222e752dfdc3b38433
sselva11 <111349018+sselva11@users.noreply.github.com>
2025-07-01 10:23:08 +0530

    sum of numbers using for loop

---
+++ sum.java
@@ -0,0 +1,14 @@
+package test;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for (int i = 1; i <= 100; i++) {
+		sum += i;
+		}
+		System.out.println("Sum of numbers from 1 to 100 is: " + sum);
+	}
+
+}
